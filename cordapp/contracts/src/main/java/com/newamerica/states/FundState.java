53cb40dd7d740b1dcc5eddf3b7684ab0582d6c08
Jonathan Scialpi <scialpi27@gmail.com>
2020-08-20 13:15:37 -0400

    created helper function for withdrawals; added documentation to the fund state; created 3 unit state unit tests

--- cordapp/contracts/src/main/java/com/newamerica/states/FundState.java
+++ cordapp/contracts/src/main/java/com/newamerica/states/FundState.java
@@ -3,28 +3,46 @@ package com.newamerica.states;
 import com.template.contracts.TemplateContract;
 import net.corda.core.contracts.BelongsToContract;
 import net.corda.core.contracts.ContractState;
+import net.corda.core.contracts.LinearState;
+import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.AbstractParty;
 import net.corda.core.identity.Party;
+import net.corda.core.serialization.ConstructorForDeserialization;
+import org.dom4j.CDATA;
+import org.jetbrains.annotations.NotNull;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Currency;
 import java.util.Date;
 import java.util.List;
 
-// *********
-// * State *
-// *********
+/**
+ * A FundState is an on-ledger representation of off-ledger recovered assets.
+ *
+ *  originCountry - the source country which will be sending the funds (US).
+ *  targetCountry - the target country which will be receiving the funds (Catan).
+ *  amount - the original value the fund held when it was issued.
+ *  balance - the current value that exists in the fund.
+ *  datetime - the day/time the fund was issued.
+ *  maxWithdrawalAmount - a term agreed upon in an off-leger bi-lateral agreement.
+ *  currency - the globally recognized currency for the fund balance and amount.
+ *  status - current stage of the fundState's lifecycle in Corda (can be ISSUED or PAID)
+ */
 @BelongsToContract(TemplateContract.class)
-public class FundState implements ContractState {
+public class FundState implements LinearState {
     public final Party originCountry;
     public final Party targetCountry;
     public final double amount;
     public final double balance;
-    public final Date datetime;
+    public final LocalDate datetime;
     public final double maxWithdrawalAmount;
     public final Currency currency;
     public final String status;
-    public FundState(Party originCountry, Party targetCountry, double amount, double balance, Date datetime, double maxWithdrawalAmount, Currency currency, String status) {
+    public final UniqueIdentifier linearId;
+
+    @ConstructorForDeserialization
+    public FundState(Party originCountry, Party targetCountry, double amount, double balance, LocalDate datetime, double maxWithdrawalAmount, Currency currency, String status, UniqueIdentifier linearId) {
         this.originCountry = originCountry;
         this.targetCountry = targetCountry;
         this.amount = amount;
@@ -33,20 +51,47 @@ public class FundState implements ContractState {
         this.maxWithdrawalAmount = maxWithdrawalAmount;
         this.currency = currency;
         this.status = status;
+        this.linearId = linearId;
     }
+
+    public FundState(Party originCountry, Party targetCountry, double amount, double balance, LocalDate datetime, double maxWithdrawalAmount, Currency currency, String status){
+        this(originCountry, targetCountry, amount, balance, datetime, maxWithdrawalAmount, currency, status, new UniqueIdentifier());
+    }
+
     //getters
     @Override
     public List<AbstractParty> getParticipants() {
         return Arrays.asList();
     }
+    @NotNull
+    @Override
+    public UniqueIdentifier getLinearId() {
+        return this.linearId;
+    }
     public Party getOriginCountry() {        return originCountry;    }
     public Party getTargetCountry() {        return targetCountry;    }
     public double getAmount() {        return amount;    }
     public double getBalance() { return balance;    }
-    public Date getDatetime() { return datetime;    }
+    public LocalDate getDatetime() { return datetime;    }
     public double getMaxWithdrawalAmount() { return maxWithdrawalAmount;    }
     public Currency getCurrency() { return currency;    }
     public String getStatus() { return status;          }
+
+    //helper functions
+
+    // return the difference between the balance and the withdrawn amount.
+    public FundState withdraw(double withdrawalAmount){
+        return new FundState(
+                this.originCountry,
+                this.targetCountry,
+                this.amount,
+                (this.balance - withdrawalAmount),
+                this.datetime,
+                this.maxWithdrawalAmount,
+                this.currency,
+                this.status
+                );
+    }
 }
 
 
---
+++ cordapp/contracts/src/test/java/com/newamerica/state/FundStateTests.java
@@ -0,0 +1,83 @@
+package com.newamerica.state;
+
+import com.newamerica.states.FundState;
+import net.corda.core.identity.Party;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.time.LocalDate;
+import java.util.Currency;
+import java.util.Date;
+import java.util.Locale;
+
+import static com.newamerica.TestUtils.CATAN;
+import static com.newamerica.TestUtils.US;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class FundStateTests {
+
+    // ensure that the FundState object has all necessary attributes and correct data types.
+    @Test
+    public void hasAllAttributes() throws NoSuchFieldException{
+        Field originCountry = FundState.class.getDeclaredField("originCountry");
+        Field targetCountry = FundState.class.getDeclaredField("targetCountry");
+        Field amount = FundState.class.getDeclaredField("amount");
+        Field balance = FundState.class.getDeclaredField("balance");
+        Field datetime = FundState.class.getDeclaredField("datetime");
+        Field maxWithdrawalAmount = FundState.class.getDeclaredField("maxWithdrawalAmount");
+        Field currency = FundState.class.getDeclaredField("currency");
+        Field status = FundState.class.getDeclaredField("status");
+
+        assertTrue(originCountry.getType().isAssignableFrom(Party.class));
+        assertTrue(targetCountry.getType().isAssignableFrom(Party.class));
+        assertTrue(amount.getType().isAssignableFrom(double.class));
+        assertTrue(balance.getType().isAssignableFrom(double.class));
+        assertTrue(datetime.getType().isAssignableFrom(LocalDate.class));
+        assertTrue(maxWithdrawalAmount.getType().isAssignableFrom(double.class));
+        assertTrue(currency.getType().isAssignableFrom(Currency.class));
+        assertTrue(status.getType().isAssignableFrom(String.class));
+    }
+
+    // ensure all getter tests return data as expected
+    @Test
+    public void getterTests(){
+        FundState fundState = new FundState(
+                US.getParty(),
+                CATAN.getParty(),
+                5000000,
+                5000000,
+                LocalDate.of(2020, 6, 27),
+                1000000,
+                Currency.getInstance(Locale.US),
+                "ISSUED"
+        );
+
+        assertEquals(fundState.getOriginCountry(), US.getParty());
+        assertEquals(fundState.getTargetCountry(), CATAN.getParty());
+        assertTrue(fundState.getAmount() > 4999999);
+        assertTrue(fundState.getBalance() > 4999999);
+        assertEquals(fundState.getDatetime(), LocalDate.of(2020, 6, 27));
+        assertTrue(fundState.getMaxWithdrawalAmount() > 999999);
+        assertEquals(fundState.getCurrency(), Currency.getInstance(Locale.US));
+        assertEquals(fundState.getStatus(), "ISSUED");
+
+    }
+
+    @Test
+    public void withdrawalHelperFunctionTest(){
+        FundState fundState = new FundState(
+                US.getParty(),
+                CATAN.getParty(),
+                5000000,
+                5000000,
+                LocalDate.of(2020, 6, 27),
+                1000000,
+                Currency.getInstance(Locale.US),
+                "ISSUED"
+        );
+        double newBlance = fundState.withdraw(1000000).getBalance();
+        assertTrue(newBlance > 3999999 && newBlance < 4000001);
+    }
+
+}
