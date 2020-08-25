46abed77f6b1f05e87f1c8f9570684d135df9377
Zhenzhen Tan <zhenzhen.tan@r3.com>
2020-08-25 13:39:59 -0400

    add partial request state and unit tests

--- cordapp/contracts/src/main/java/com/newamerica/states/PartialRequestState.java
+++ cordapp/contracts/src/main/java/com/newamerica/states/PartialRequestState.java
@@ -1,7 +1,5 @@
 package com.newamerica.states;
 
-import com.newamerica.contracts.RequestContract;
-import net.corda.core.contracts.BelongsToContract;
 import net.corda.core.contracts.LinearState;
 import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.AbstractParty;
@@ -15,7 +13,7 @@ import java.util.Currency;
 import java.util.List;
 
 /**
- * A RequestState is an on-ledger representation of request data that gets stored in the database.
+ * A PartialRequestState is an on-ledger representation of partial request data that gets stored in the database.
  *
  *  authorizedUserUsername - The name of a Catan MoFA approved authorized user, who sends the request
  *  authorizedUserDept - the department name that the authorized user belongs to
@@ -27,60 +25,48 @@ import java.util.List;
  *  status - current stage of the requestState's lifecycle in Corda (can be ISSUED, PENDING, FlAGGED)
  *  fundStateLinearId -  A reference to the fund state that this request is based on
  */
-@BelongsToContract(RequestContract.class)
-public class RequestState implements LinearState {
-    public final String authorizedUserUsername;
+public class PartialRequestState implements LinearState {
+
     public final String authorizedUserDept;
-    public final String authorizerUserUsername;
     public final Party authorizerDept;
     public final String externalAccountId;
     public final BigDecimal amount;
     public final Currency currency;
     public final ZonedDateTime datetime;
-    public final RequestStateStatus status;
     public final UniqueIdentifier fundStateLinearId;
     public final UniqueIdentifier linearId;
     public final List<AbstractParty> participants;
 
     @ConstructorForDeserialization
-    public RequestState(String authorizedUserUsername,
-                        String authorizedUserDept,
-                        String authorizerUserUsername,
+    public PartialRequestState(String authorizedUserDept,
                         Party authorizerDept,
                         String externalAccountId,
                         BigDecimal amount,
                         Currency currency,
                         ZonedDateTime datetime,
-                        RequestStateStatus status,
                         UniqueIdentifier fundStateLinearId,
                         UniqueIdentifier linearId,
                         List<AbstractParty> participants) {
-        this.authorizedUserUsername = authorizedUserUsername;
         this.authorizedUserDept = authorizedUserDept;
-        this.authorizerUserUsername = authorizerUserUsername;
         this.authorizerDept = authorizerDept;
         this.externalAccountId = externalAccountId;
         this.amount = amount;
         this.currency = currency;
         this.datetime = datetime;
-        this.status = status;
         this.fundStateLinearId = fundStateLinearId;
         this.linearId = linearId;
         this.participants = participants;
     }
 
-    public RequestState(String authorizedUserUsername,
-                        String authorizedUserDept,
-                        String authorizerUsername,
+    public PartialRequestState(String authorizedUserDept,
                         Party authorizerDept,
                         String externalAccount,
                         BigDecimal amount,
                         Currency currency,
                         ZonedDateTime datetime,
-                        RequestStateStatus status,
                         UniqueIdentifier fundStateLinearId,
                         List<AbstractParty> participants) {
-        this(authorizedUserUsername, authorizedUserDept, authorizerUsername, authorizerDept, externalAccount, amount, currency, datetime, status, fundStateLinearId, new UniqueIdentifier(), participants);
+        this(authorizedUserDept, authorizerDept, externalAccount, amount, currency, datetime, fundStateLinearId, new UniqueIdentifier(), participants);
     }
 
 
@@ -93,42 +79,12 @@ public class RequestState implements LinearState {
     public List<AbstractParty> getParticipants() { return participants;}
 
     //getters
-    public String getAuthorizedUserUsername() { return authorizedUserUsername; }
     public String getAuthorizedUserDept() { return authorizedUserDept; }
     public Party getAuthorizerDept() { return authorizerDept; }
     public BigDecimal getAmount() { return amount; }
     public Currency getCurrency() { return currency; }
     public ZonedDateTime getDatetime() { return datetime; }
-    public RequestStateStatus getStatus() { return status; }
     public UniqueIdentifier getFundStateLinearId() { return fundStateLinearId; }
-    public String getAuthorizerUserUsername() { return authorizerUserUsername; }
     public String getExternalAccountId() { return externalAccountId; }
 
-    //helper functions
-    public RequestState changeStatus(RequestStateStatus newStatus){
-        return new RequestState(
-                this.authorizedUserUsername,
-                this.authorizedUserDept,
-                this.authorizerUserUsername,
-                this.authorizerDept,
-                this.externalAccountId,
-                this.amount,
-                this.currency,
-                this.datetime,
-                newStatus,
-                this.fundStateLinearId,
-                this.participants
-        );
-    }
-
-    public enum RequestStateStatus {
-        PENDING("pending"),
-        FLAGGED("flagged"),
-        APPROVED("approved");
-
-        public final String status;
-        RequestStateStatus(String status) {
-            this.status = status;
-        }
-    }
 }
