9d70e988d6bdd87bf3daad7ced573ddbb020a51f
Neal Shah <neal.shah@r3.com>
2020-08-28 08:29:50 +0100

    Initial commit and changes for dockerising client

---
+++ cordapp/clients/docker-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+TAG="${1}"
+PASSWORD="${2}"
+PUSH="${3}"
+
+IMAGE="newamericapocacr.azurecr.io/oars-client"
+
+if [[ "$(docker images -q "${IMAGE}":"${TAG}" 2> /dev/null)" == "" ]]; then
+    docker build -t ${IMAGE}:${TAG} -t ${IMAGE}:latest .
+fi
+
+if [ "${PUSH}" = true ] ; then
+    docker login -u newamericapocacr -p "${PASSWORD}" newamericapocacr.azurecr.io
+    docker push newamericapocacr.azurecr.io/oars-client
+fi
