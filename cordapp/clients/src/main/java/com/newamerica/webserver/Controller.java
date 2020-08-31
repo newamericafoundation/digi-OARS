312cf0ec1f3c508b9931438925315c02933d266d
Zhenzhen Tan <zhenzhen.tan@r3.com>
2020-08-31 10:55:07 -0400

    merge master

--- cordapp/clients/src/main/java/com/newamerica/webserver/Controller.java
+++ cordapp/clients/src/main/java/com/newamerica/webserver/Controller.java
@@ -1,14 +1,12 @@
 package com.newamerica.webserver;
 
 import net.corda.core.messaging.CordaRPCOps;
-import net.corda.core.node.services.vault.QueryCriteria;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +30,12 @@ public class Controller {
 
     @GetMapping(value = "/nodeInfo", produces = "application/json")
     private ResponseEntity<String> getNodeInfo() {
+        return new ResponseEntity<String>(proxy.nodeInfo().getLegalIdentities().toString(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/fund", produces = "application/json")
+    private ResponseEntity<String> createFund() {
+
         return new ResponseEntity<String>(proxy.nodeInfo().getLegalIdentities().toString(), HttpStatus.OK);
     }
 }
