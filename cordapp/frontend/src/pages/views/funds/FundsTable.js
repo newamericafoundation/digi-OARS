e8234cd2bfffec4042e7f23749fcf7188e055af8
Neal Shah <neal.shah@r3.com>
2020-08-31 17:52:10 +0100

    Updated money formats and date formats

--- cordapp/frontend/src/pages/views/funds/FundsTable.js
+++ cordapp/frontend/src/pages/views/funds/FundsTable.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import {
+  CCard,
+  CCardHeader,
   CBadge,
   CButton,
   CCardBody,
   CDataTable,
   CCollapse,
+  CCol,
+  CRow,
 } from "@coreui/react";
+import Moment from 'moment';
 import { FundData } from "../../../data/Funds";
 
 export const FundsTable = () => {
@@ -24,14 +29,12 @@ export const FundsTable = () => {
   };
 
   const fields = [
-    { key: "linearId" },
     { key: "originCountry" },
     { key: "receivingCountry" },
     { key: "amount" },
     { key: "balance" },
-    { key: "datetime" },
+    { key: "datetime", label: "Date" },
     { key: "maxWithdrawalAmount" },
-    { key: "currency" },
     { key: "status", _style: { width: "20%" } },
     {
       key: "show_details",
@@ -42,7 +45,7 @@ export const FundsTable = () => {
     },
   ];
 
-  const getBadge = (status) => {
+  const getStatusBadge = (status) => {
     switch (status) {
       case "RECEIVED":
         return "success";
@@ -51,6 +54,43 @@ export const FundsTable = () => {
       default:
         return "primary";
     }
+  };
+
+  const getReceivedButton = (isReceived) => {
+    if (isReceived) {
+      return (
+        <CButton
+          className={"float-right mb-0"}
+          color="dark"
+          variant="outline"
+          shape="square"
+          size="sm"
+          active={false}
+          disabled={true}
+        >
+          Received
+        </CButton>
+      );
+    }
+    return (
+      <CButton
+        className={"float-right mb-0"}
+        color="success"
+        variant="outline"
+        shape="square"
+        size="sm"
+        active={true}
+      >
+        Receive
+      </CButton>
+    );
+  };
+
+  const toCurrency = (number, currency) => {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: currency,
+    }).format(number);
   };
 
   return (
@@ -66,14 +106,23 @@ export const FundsTable = () => {
       sorter
       pagination
       scopedSlots={{
+        amount: (item) => <td>{toCurrency(item.amount, item.currency)}</td>,
+        balance: (item) => <td>{toCurrency(item.balance, item.currency)}</td>,
+        maxWithdrawalAmount: (item) => (
+          <td>{toCurrency(item.maxWithdrawalAmount, item.currency)}</td>
+        ),
+        datetime: (item) => (
+        <td>{Moment(item.datetime).format('DD/MMM/yyyy')}</td>
+        ),
         status: (item) => (
           <td>
-            <CBadge color={getBadge(item.status)}>{item.status}</CBadge>
+            <CBadge color={getStatusBadge(item.status)}>{item.status}</CBadge>
           </td>
         ),
+        isReceived: (item) => <td>{getReceivedButton(item.isReceived)}</td>,
         show_details: (item, index) => {
           return (
-            <td className="py-2">
+            <td>
               <CButton
                 color="primary"
                 variant="outline"
@@ -91,22 +140,42 @@ export const FundsTable = () => {
         details: (item, index) => {
           return (
             <CCollapse show={details.includes(index)}>
-              <CCardBody>
-                <h4>Linear Id: {item.linearId}</h4>
-                <p className="text-muted">
-                  Origin Country: {item.originCountry}
-                  <br />
-                  Receiving Country: {item.receivingCountry}
-                  <br />
-                  Amount ({item.currency}): {item.amount}
-                  <br />
-                  Balance ({item.currency}): {item.balance}
-                  <br />
-                  Max Withdrawal Amount ({item.currency}):{" "}
-                  {item.maxWithdrawalAmount}
-                  <br />
-                </p>
-              </CCardBody>
+              <CCard className="m-3">
+                <CCardHeader>
+                  Fund Details
+                  {getReceivedButton(item.isReceived)}
+                </CCardHeader>
+                <CCardBody>
+                  <CRow>
+                    <CCol md="3">
+                      ID:
+                      <br />
+                      Origin Country:
+                      <br />
+                      Receiving Country:
+                      <br />
+                      Amount:
+                      <br />
+                      Balance:
+                      <br />
+                      Max Withdrawal Amount
+                    </CCol>
+                    <CCol md="3">
+                      {item.linearId}
+                      <br />
+                      {item.originCountry}
+                      <br />
+                      {item.receivingCountry}
+                      <br />
+                      {toCurrency(item.amount, item.currency)}
+                      <br />
+                      {toCurrency(item.balance, item.currency)}
+                      <br />
+                      {toCurrency(item.maxWithdrawalAmount, item.currency)}
+                    </CCol>
+                  </CRow>
+                </CCardBody>
+              </CCard>
             </CCollapse>
           );
         },
