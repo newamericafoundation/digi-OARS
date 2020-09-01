ac59ef6cfc394364e77e052d4e77abb37e240c5b
Neal Shah <neal.shah@r3.com>
2020-09-01 07:44:39 +0100

    Added requests and transfers mock data

---
+++ cordapp/frontend/src/data/Transfers.js
@@ -0,0 +1,15 @@
+export const TransferData = [
+	{
+	  issuanceParty: "Catan Treasury",
+	  receivingDept: "Catan Ministry of Immigration",
+	  authorizedUserUsername: "Chris Blue",
+	  authorizerDept: "Catan Ministry of Justice",
+	  amount: "1000000",
+	  currency: "USD",
+	  externalAccountId: "88888888",
+	  purpose: "implement a new project",
+	  datetime: "2020-02-20T08:12:39.483Z",
+	  requestStateLinearId: "1601534946",
+	  linearId: "1601534947"
+	}
+]
