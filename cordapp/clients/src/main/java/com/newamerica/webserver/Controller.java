390b83dad153f51c5d6caf220856c3cc08c1c806
Zhenzhen Tan <zhenzhen.tan@r3.com>
2020-09-01 11:40:45 -0400

    pull remote

--- cordapp/clients/src/main/java/com/newamerica/webserver/Controller.java
+++ cordapp/clients/src/main/java/com/newamerica/webserver/Controller.java
@@ -11,10 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
@@ -70,7 +67,7 @@ public class Controller {
     }
 
     @PostMapping(value = "fund", produces = "application/json")
-    private ResponseEntity<String> createFund(HttpServletRequest request) {
+    private ResponseEntity<String> createFund (@RequestBody request) {
         String originPartyName = request.getParameter("originParty");
         String receivingPartyName = request.getParameter("receivingParty");
         String amountStr = request.getParameter("amount");
---
+++ cordapp/clients/src/main/java/com/newamerica/webserver/objects/Fund.java
@@ -0,0 +1,5 @@
+package com.newamerica.webserver.objects;
+
+
+public class Fund {
+}
