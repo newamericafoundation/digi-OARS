34b4a6007d68bb19583a45c2b948baf69a894252
Neal Shah <neal.shah@r3.com>
2020-09-08 08:42:47 +0100

    Added get request by id and status

--- cordapp/clients/src/main/java/com/newamerica/webserver/CommonController.java
+++ cordapp/clients/src/main/java/com/newamerica/webserver/CommonController.java
@@ -1,43 +1,14 @@
 package com.newamerica.webserver;
 
-import com.newamerica.flows.IssueFundFlow;
-import com.newamerica.flows.IssueRequestFlow;
-import com.newamerica.flows.ReceiveFundFlow;
-import com.newamerica.states.FundState;
-import com.newamerica.states.RequestState;
-import com.newamerica.webserver.dtos.Fund;
-import com.newamerica.webserver.dtos.Request;
-import net.corda.core.contracts.StateAndRef;
-import net.corda.core.contracts.UniqueIdentifier;
-import net.corda.core.identity.AbstractParty;
-import net.corda.core.identity.CordaX500Name;
 import net.corda.core.identity.Party;
 import net.corda.core.messaging.CordaRPCOps;
-import net.corda.core.node.services.Vault;
-import net.corda.core.node.services.vault.PageSpecification;
-import net.corda.core.node.services.vault.QueryCriteria;
-import net.corda.core.transactions.SignedTransaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.StreamingOutput;
-import java.math.BigDecimal;
-import java.time.ZonedDateTime;
-import java.util.Arrays;
-import java.util.Currency;
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
-
-import static net.corda.core.node.services.vault.QueryCriteriaUtils.DEFAULT_PAGE_NUM;
 
 /**
  * Define your API endpoints here.
