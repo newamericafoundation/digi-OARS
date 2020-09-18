524bbffcc1a6be79e3391488de4fef69c463d6ef
Neal Shah <neal.shah@r3.com>
2020-09-18 09:44:52 +0100

    Attempt at fixing routes

--- cordapp/frontend/src/routes/index.js
+++ cordapp/frontend/src/routes/index.js
@@ -1,31 +1,43 @@
-import React from 'react';
-import { useKeycloak } from '@react-keycloak/web';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { useKeycloak } from "@react-keycloak/web";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { publicRoutes, privateRoutes } from "../routes";
 
 const loading = (
-    <div className="pt-3 text-center">
-        <div className="sk-spinner sk-spinner-pulse"></div>
-    </div>
-)
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+);
 
-const Layout = React.lazy(() => import('../containers/Layout'));
+const Layout = React.lazy(() => import("../containers/Layout"));
 // const Login = React.lazy(() => import('../views/Login'));
 
-export const AppRouter = () => {
-    const [initialized] = useKeycloak()
+const routes = [...publicRoutes, ...privateRoutes];
 
-    if (!initialized) {
-        return loading
-    }
-    
-    return (
-        <BrowserRouter>
-            <React.Suspense fallback={loading}>
-                <Switch>
-                    <Route path="/" name="Home" render={props => <Layout {...props} />}></Route>
-                    {/* <Route path="/login" name="Login" render={props => <Login {...props} />}/> */}
-                </Switch>
-            </React.Suspense>
-        </BrowserRouter>
-    );
-}
+export const AppRouter = () => {
+  const [initialized] = useKeycloak();
+
+  if (!initialized) {
+    return loading;
+  }
+
+  return (
+    <BrowserRouter>
+      <React.Suspense fallback={loading}>
+        <Switch>
+          {routes.map((route, idx) => (
+              route.component &&
+            <Route
+              key={idx}
+              path={route.path}
+              name={route.name}
+              exact={route.exact}
+              render={() => <Layout {...route.component} />}
+            />
+          ))}
+          {/* <Route path="/login" name="Login" render={props => <Login {...props} />}/> */}
+        </Switch>
+      </React.Suspense>
+    </BrowserRouter>
+  );
+};
