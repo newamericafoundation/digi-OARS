dfaeb20f6b36f040b344c54d2b4633838685fd68
Zhenzhen Tan <zhenzhen.tan@r3.com>
2020-09-21 14:39:44 -0400

    add List<AbstractParty>

--- cordapp/workflows/src/main/java/com/newamerica/flows/IssueFundFlow.java
+++ cordapp/workflows/src/main/java/com/newamerica/flows/IssueFundFlow.java
@@ -32,7 +32,7 @@ public class IssueFundFlow {
     public static class InitiatorFlow extends FlowLogic<SignedTransaction>{
         private final FundState outputFundState;
 
-        public InitiatorFlow(Party originParty, Party receivingParty, List<AbstractParty> owners, List<AbstractParty> requiredSigners, List<AbstractParty> partialRequestParticipants, BigDecimal amountAndBalance, ZonedDateTime createDatetime, ZonedDateTime updateDatetime, BigDecimal maxWithdrawalAmount, Currency currency, List participants){
+        public InitiatorFlow(Party originParty, Party receivingParty, List<AbstractParty> owners, List<AbstractParty> requiredSigners, List<AbstractParty> partialRequestParticipants, BigDecimal amountAndBalance, ZonedDateTime createDatetime, ZonedDateTime updateDatetime, BigDecimal maxWithdrawalAmount, Currency currency, List<AbstractParty> participants){
             this.outputFundState = new FundState(originParty,receivingParty, owners, requiredSigners, partialRequestParticipants, amountAndBalance, amountAndBalance, createDatetime, updateDatetime, maxWithdrawalAmount, currency, FundState.FundStateStatus.ISSUED, participants);
         }
 
