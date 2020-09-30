06e6952e3c7327838e5374a22078e59271eb611f
Neal Shah <neal.shah@r3.com>
2020-09-30 08:21:50 +0100

    Amended getTransfers for new API, implemented single table with filtering button group for approved and transferred withdrawal requests

--- cordapp/frontend/src/data/GetTransfers.js
+++ cordapp/frontend/src/data/GetTransfers.js
@@ -6,17 +6,16 @@ const getRequests = async (port) => {
 
   const res = await axios.get(url);
   return res.data.entity.map((ob, index) => ({
-    issuanceParty: ob.state.data.issuanceParty,
-    receivingDept: ob.state.data.receivingDept,
-    authorizedUserUsername: ob.state.data.authorizedUserUsername,
-    externalAccountId: ob.state.data.externalAccountId,
-    linearId: ob.state.data.linearId.id,
-    requestStateLinearId: ob.state.data.requestStateLinearId.id,
-    currency: ob.state.data.currency,
-    amount: ob.state.data.amount,
-    participants: ob.state.data.participants,
-    txId: ob.ref.txhash,
-    createDateTime: ob.state.data.datetime
+    issuanceParty: ob.issuanceParty,
+    receivingDept: ob.receivingDept,
+    authorizedUserUsername: ob.authorizedUserUsername,
+    externalAccountId: ob.externalAccountId,
+    linearId: ob.linearId.id,
+    requestStateLinearId: ob.requestStateLinearId.id,
+    currency: ob.currency,
+    amount: ob.amount,
+    participants: ob.participants,
+    createDateTime: ob.datetime
   }));
 };
 
