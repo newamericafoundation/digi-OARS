b80c72978e4a509c2d92d93d62e58437e4b77ec3
Neal Shah <neal.shah@r3.com>
2020-10-01 10:38:16 +0100

    Added modal confirmation to Receive funds

--- cordapp/frontend/src/pages/views/funds/FundsTable.js
+++ cordapp/frontend/src/pages/views/funds/FundsTable.js
@@ -13,6 +13,11 @@ import {
   CCallout,
   CSpinner,
   CTooltip,
+  CModal,
+  CModalHeader,
+  CModalTitle,
+  CModalBody,
+  CModalFooter
 } from "@coreui/react";
 import moment from "moment-timezone";
 import axios from "axios";
@@ -24,6 +29,19 @@ export const FundsTable = ({ funds, isReceiver, refreshTableCallback }) => {
   const [api] = useContext(APIContext);
   const [details, setDetails] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [show, setShow] = useState(false);
+  const [currentItem, setCurrentItem] = useState({});
+  const [currentItemIndex, setCurrentItemIndex] = useState();
+  const [currentRequestAction, setCurrentRequestAction] = useState("");
+
+  const handleShow = (item) => {
+    setCurrentItem(item);
+    setShow(true);
+  };
+
+  const handleClose = () => {
+    setShow(false);
+  };
 
   const toggleDetails = (index) => {
     const position = details.indexOf(index);
@@ -37,6 +55,7 @@ export const FundsTable = ({ funds, isReceiver, refreshTableCallback }) => {
   };
 
   const fields = [
+    { key: "originParty", label: "Origin Country" },
     { key: "receivingParty", label: "Receiving Country" },
     { key: "amount" },
     { key: "balance" },
@@ -66,30 +85,42 @@ export const FundsTable = ({ funds, isReceiver, refreshTableCallback }) => {
     }
   };
 
-  const getReceivedButton = (status, id, index) => {
-    if (isReceiver) {
-      if (status === "ISSUED") {
-        return (
-          <CButton
-            className={"float-left mb-0"}
-            color="success"
-            variant="outline"
-            shape="square"
-            size="sm"
-            onClick={() => onHandleReceiveClick(id, index)}
-          >
-            {isLoading ? (
-              <CSpinner
-                className="spinner-border spinner-border-sm mr-1"
-                role="status"
-                aria-hidden="true"
-              />
-            ) : null}
-            Receive
-          </CButton>
-        );
-      }
+  const getActionButton = (item) => {
+    if (item.status === "ISSUED" && isReceiver) {
+      return (
+        <CButton
+          className={"float-left mb-0"}
+          color="success"
+          variant="outline"
+          shape="square"
+          size="sm"
+          onClick={() => {
+            setCurrentRequestAction("receive");
+            handleShow(item);
+          }}
+        >
+          Receive
+        </CButton>
+      );
     }
+  };
+
+  const onHandleConfirmationClick = (
+    fundId
+  ) => {
+    setIsLoading(true);
+    const url =
+      "http://" + window._env_.API_CLIENT_URL + ":" + api.port + "/api/fund";
+
+    axios
+      .put(url, null, { params: { fundId } })
+      .then((response) => {
+        setIsLoading(false);
+        refreshTableCallback();
+        handleClose();
+        toggleDetails(currentItemIndex);
+      })
+      .catch((err) => console.log(err));
   };
 
   const onHandleReceiveClick = (fundId, index) => {
@@ -108,167 +139,238 @@ export const FundsTable = ({ funds, isReceiver, refreshTableCallback }) => {
   };
 
   return (
-    <CDataTable
-      items={funds}
-      fields={fields}
-      columnFilter
-      tableFilter
-      itemsPerPageSelect
-      itemsPerPage={5}
-      clickableRows
-      hover
-      sorter
-      pagination
-      scopedSlots={{
-        originParty: (item) => (
-          <td>{toCountryByIsoFromX500(item.originParty)}</td>
-        ),
-        receivingParty: (item) => (
-          <td>{toCountryByIsoFromX500(item.receivingParty)}</td>
-        ),
-        amount: (item) => <td>{toCurrency(item.amount, item.currency)}</td>,
-        balance: (item) => <td>{toCurrency(item.balance, item.currency)}</td>,
-        maxWithdrawalAmount: (item) => (
-          <td>{toCurrency(item.maxWithdrawalAmount, item.currency)}</td>
-        ),
-        createdDateTime: (item) => (
-          <td>
-            {moment
-              .tz(item.createdDateTime, "UTC")
-              .format(Constants.DATE_FORMAT)}
-          </td>
-        ),
-        updatedDateTime: (item) => (
-          <td>
-            {moment
-              .tz(item.updateDateTime, "UTC")
-              .format(Constants.DATE_FORMAT)}
-          </td>
-        ),
-        status: (item) => (
-          <td>
-            <CBadge color={getStatusBadge(item.status)}>{item.status}</CBadge>
-          </td>
-        ),
-        actions: (item, index) => {
-          return (
-            <td>{getReceivedButton(item.status, item.linearId, index)}</td>
-          );
-        },
-        show_details: (item, index) => {
-          return (
+    <>
+      <CDataTable
+        items={funds}
+        fields={fields}
+        columnFilter
+        tableFilter
+        itemsPerPageSelect
+        itemsPerPage={5}
+        clickableRows
+        hover
+        sorter
+        pagination
+        scopedSlots={{
+          originParty: (item) => (
+            <td>{toCountryByIsoFromX500(item.originParty)}</td>
+          ),
+          receivingParty: (item) => (
+            <td>{toCountryByIsoFromX500(item.receivingParty)}</td>
+          ),
+          amount: (item) => <td>{toCurrency(item.amount, item.currency)}</td>,
+          balance: (item) => <td>{toCurrency(item.balance, item.currency)}</td>,
+          maxWithdrawalAmount: (item) => (
+            <td>{toCurrency(item.maxWithdrawalAmount, item.currency)}</td>
+          ),
+          createdDateTime: (item) => (
             <td>
-              <CButton
-                color="primary"
-                variant="outline"
-                shape="square"
-                size="sm"
-                onClick={() => {
-                  toggleDetails(index);
-                }}
-              >
-                {details.includes(index) ? "Hide" : "Show"}
-              </CButton>
+              {moment
+                .tz(item.createdDateTime, "UTC")
+                .format(Constants.DATE_FORMAT)}
             </td>
-          );
-        },
-        details: (item, index) => {
-          return (
-            <CCollapse show={details.includes(index)}>
-              <CCard className="m-3">
-                <CCardHeader>
-                  Fund Details
-                </CCardHeader>
-                <CCardBody>
-                  {item.status === Constants.FUND_RECEIVED ? (
-                    <CRow className="mb-3">
-                      <CCol>
-                        <p className="text-muted">Total Assets Repatriated:</p>
-                        <CProgress
-                          value={(item.balance / item.amount) * 100}
-                          showPercentage
-                          striped
-                          color="success"
-                          precision={2}
-                        />
-                      </CCol>
-                    </CRow>
-                  ) : null}
-                  <CRow>
-                    <CCol xl="4" sm="3">
-                      <CTooltip content={item.linearId} placement="right-end">
+          ),
+          updatedDateTime: (item) => (
+            <td>
+              {moment
+                .tz(item.updateDateTime, "UTC")
+                .format(Constants.DATE_FORMAT)}
+            </td>
+          ),
+          status: (item) => (
+            <td>
+              <CBadge color={getStatusBadge(item.status)}>{item.status}</CBadge>
+            </td>
+          ),
+          actions: (item) => {
+            return (
+              <td>{getActionButton(item)}</td>
+            );
+          },
+          show_details: (item, index) => {
+            return (
+              <td>
+                <CButton
+                  color="primary"
+                  variant="outline"
+                  shape="square"
+                  size="sm"
+                  onClick={() => {
+                    toggleDetails(index);
+                  }}
+                >
+                  {details.includes(index) ? "Hide" : "Show"}
+                </CButton>
+              </td>
+            );
+          },
+          details: (item, index) => {
+            return (
+              <CCollapse show={details.includes(index)}>
+                <CCard className="m-3">
+                  <CCardHeader>Fund Details</CCardHeader>
+                  <CCardBody>
+                    {item.status === Constants.FUND_RECEIVED ? (
+                      <CRow className="mb-3">
+                        <CCol>
+                          <p className="text-muted">
+                            Total Assets Repatriated:
+                          </p>
+                          <CProgress
+                            value={(item.balance / item.amount) * 100}
+                            showPercentage
+                            striped
+                            color="success"
+                            precision={2}
+                          />
+                        </CCol>
+                      </CRow>
+                    ) : null}
+                    <CRow>
+                      <CCol xl="4" sm="3">
+                        <CTooltip content={item.linearId} placement="right-end">
+                          <CCallout color="info" className={"bg-light"}>
+                            <p className="text-muted mb-0">State ID</p>
+                            <strong className="p">{item.linearId}</strong>
+                          </CCallout>
+                        </CTooltip>
                         <CCallout color="info" className={"bg-light"}>
-                          <p className="text-muted mb-0">State ID</p>
-                          <strong className="p">{item.linearId}</strong>
+                          <p className="text-muted mb-0">Origin Country</p>
+                          <strong className="p">
+                            {toCountryByIsoFromX500(item.originParty)}
+                          </strong>
                         </CCallout>
-                      </CTooltip>
-                      <CCallout color="info" className={"bg-light"}>
-                        <p className="text-muted mb-0">Origin Country</p>
-                        <strong className="p">
-                          {toCountryByIsoFromX500(item.originParty)}
-                        </strong>
-                      </CCallout>
-                      <CCallout color="info" className={"bg-light"}>
-                        <p className="text-muted mb-0">Receiving Country</p>
-                        <strong className="p">
-                          {toCountryByIsoFromX500(item.receivingParty)}
-                        </strong>
-                      </CCallout>
-                    </CCol>
-                    <CCol xl="4" sm="3">
-                      <CCallout color="info" className={"bg-light"}>
-                        <p className="text-muted mb-0">Amount</p>
-                        <strong className="p">
-                          {toCurrency(item.amount, item.currency)}
-                        </strong>
-                      </CCallout>
-                      <CCallout color="info" className={"bg-light"}>
-                        <p className="text-muted mb-0">Balance</p>
-                        <strong className="p">
-                          {toCurrency(item.balance, item.currency)}
-                        </strong>
-                      </CCallout>
-                      <CCallout color="info" className={"bg-light"}>
-                        <p className="text-muted mb-0">Max Withdrawal Amount</p>
-                        <strong className="p">
-                          {toCurrency(item.maxWithdrawalAmount, item.currency)}
-                        </strong>
-                      </CCallout>
-                    </CCol>
-                    <CCol xl="4" sm="3">
-                      <CCallout
-                        color={item.status === "ISSUED" ? "warning" : "success"}
-                        className={"bg-light"}
-                      >
-                        <p className="text-muted mb-0">Status</p>
-                        <strong className="p">{item.status}</strong>
-                      </CCallout>
-                      <CCallout color="info" className={"bg-light"}>
-                        <p className="text-muted mb-0">Created Date/Time</p>
-                        <strong className="p">
-                          {moment
-                            .tz(item.createdDateTime, "UTC")
-                            .format(Constants.DATETIME_FORMAT)}
-                        </strong>
-                      </CCallout>
-                      {item.createdDateTime !== item.updatedDateTime ?? (
                         <CCallout color="info" className={"bg-light"}>
-                          <p className="text-muted mb-0">Updated Date/Time</p>
+                          <p className="text-muted mb-0">Receiving Country</p>
+                          <strong className="p">
+                            {toCountryByIsoFromX500(item.receivingParty)}
+                          </strong>
+                        </CCallout>
+                      </CCol>
+                      <CCol xl="4" sm="3">
+                        <CCallout color="info" className={"bg-light"}>
+                          <p className="text-muted mb-0">Amount</p>
+                          <strong className="p">
+                            {toCurrency(item.amount, item.currency)}
+                          </strong>
+                        </CCallout>
+                        <CCallout color="info" className={"bg-light"}>
+                          <p className="text-muted mb-0">Balance</p>
+                          <strong className="p">
+                            {toCurrency(item.balance, item.currency)}
+                          </strong>
+                        </CCallout>
+                        <CCallout color="info" className={"bg-light"}>
+                          <p className="text-muted mb-0">
+                            Max Withdrawal Amount
+                          </p>
+                          <strong className="p">
+                            {toCurrency(
+                              item.maxWithdrawalAmount,
+                              item.currency
+                            )}
+                          </strong>
+                        </CCallout>
+                      </CCol>
+                      <CCol xl="4" sm="3">
+                        <CCallout
+                          color={
+                            item.status === "ISSUED" ? "warning" : "success"
+                          }
+                          className={"bg-light"}
+                        >
+                          <p className="text-muted mb-0">Status</p>
+                          <strong className="p">{item.status}</strong>
+                        </CCallout>
+                        <CCallout color="info" className={"bg-light"}>
+                          <p className="text-muted mb-0">Created Date/Time</p>
                           <strong className="p">
                             {moment
-                              .tz(item.updateDateTime, "UTC")
+                              .tz(item.createdDateTime, "UTC")
                               .format(Constants.DATETIME_FORMAT)}
                           </strong>
                         </CCallout>
-                      )}
-                    </CCol>
-                  </CRow>
-                </CCardBody>
-              </CCard>
-            </CCollapse>
-          );
-        },
-      }}
-    />
+                        {item.createdDateTime !== item.updatedDateTime ?? (
+                          <CCallout color="info" className={"bg-light"}>
+                            <p className="text-muted mb-0">Updated Date/Time</p>
+                            <strong className="p">
+                              {moment
+                                .tz(item.updateDateTime, "UTC")
+                                .format(Constants.DATETIME_FORMAT)}
+                            </strong>
+                          </CCallout>
+                        )}
+                      </CCol>
+                    </CRow>
+                  </CCardBody>
+                </CCard>
+              </CCollapse>
+            );
+          },
+        }}
+      />
+      <CModal
+        color="success"
+        show={show}
+        onClose={handleClose}
+      >
+        <CModalHeader closeButton>
+          <CModalTitle>
+            {currentRequestAction.charAt(0).toUpperCase() +
+              currentRequestAction.slice(1)}{" "}
+            Confirmation
+          </CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <CCallout
+            color="success"
+            className={"bg-light"}
+          >
+            <p className="text-muted mb-0">Request State ID</p>
+            <strong className="p">{currentItem.linearId}</strong>
+          </CCallout>
+          <CCallout
+            color="success"
+            className={"bg-light"}
+          >
+            <p className="text-muted mb-0">Origin Country</p>
+            <strong className="p">{currentItem.originParty ? toCountryByIsoFromX500(currentItem.originParty) : null}</strong>
+          </CCallout>
+          <CCallout
+            color="success"
+            className={"bg-light"}
+          >
+            <p className="text-muted mb-0">Amount</p>
+            <strong className="p">
+              {toCurrency(currentItem.amount, "USD")}
+            </strong>
+          </CCallout>
+        </CModalBody>
+        <CModalFooter>
+          <CButton
+            color="success"
+            onClick={() =>
+              onHandleConfirmationClick(
+                currentItem.linearId
+              )
+            }
+          >
+            {isLoading ? (
+              <CSpinner
+                className="spinner-border spinner-border-sm mr-1"
+                role="status"
+                aria-hidden="true"
+              />
+            ) : null}
+            {currentRequestAction.charAt(0).toUpperCase() +
+              currentRequestAction.slice(1)}{" "}
+            Funds
+          </CButton>
+          <CButton color="secondary" onClick={handleClose}>
+            Cancel
+          </CButton>
+        </CModalFooter>
+      </CModal>
+    </>
   );
 };
