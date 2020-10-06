14efd080e0250352fa2cc3d9412dbbf66b00d61f
Neal Shah <neal.shah@r3.com>
2020-10-06 15:09:17 +0100

    Added Actions view to homepage

--- cordapp/frontend/src/pages/HomePage.js
+++ cordapp/frontend/src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth } from "../auth-hook";
+import { Actions } from "../pages/views/home/Actions";
 import { Widgets } from "../pages/views/home/Widgets";
 import { Jumbotron } from "../pages/views/home/Jumbotron";
 import { WelcomeCallout } from "../pages/views/home/WelcomeCallout";
@@ -11,6 +12,9 @@ const HomePage = () => {
     <>
       <Jumbotron auth={auth} />
       <WelcomeCallout auth={auth} />
+      {auth.meta.keycloak.hasResourceRole("funds_receiver") ? (
+        <Actions />
+      ) : null}
       <Widgets auth={auth} />
     </>
   );
